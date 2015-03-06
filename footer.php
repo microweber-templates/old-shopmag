a2f92670609741318a60f863f83eb1654a1f54f2
Peter Ivanov <peter@microweber.com>
2015-03-06 21:57:04 +0200

    update

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
                   <div class="edit nodrop" rel="global" field="footer-copy"><module type="menu" template="footer" id="footer-menu" menu-name="footer_menu_about"></div>
               </div>
               <div class="mw-ui-col text-right" >
-                  <span id="mw">Powered by <a href="http://microweber.com" target="_blank" title="Drag & Drop CMS & Online Shop">Microweber</a></span>&nbsp;&nbsp;<small>|</small>&nbsp;&nbsp;<a href="<?php print admin_url(); ?>">Admin Login</a>
+                  <span id="mw"><?php print powered_by_link(); ?></span>&nbsp;&nbsp;<small>|</small>&nbsp;&nbsp;<a href="<?php print admin_url(); ?>">Admin Login</a>
               </div>
           </div>
 
