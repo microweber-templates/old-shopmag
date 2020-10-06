b1ec612ed1a092e3ed85366bec470aad78880e6a
Petko Yovchevski <petko@microweber.com>
2020-10-06 14:50:31 +0300

    Incrementing version number to 1.3

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $config = array();
 $config['name'] = "ShopMag";
 $config['description'] = "";
 $config['author'] = "Microweber";
-$config['version'] = 1.2;
+$config['version'] = 1.3;
 $config['standalone_module_skins'] = true;
